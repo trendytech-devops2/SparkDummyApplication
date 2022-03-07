a6a2ce458345ced308f54511bf358296a3b066d1
Nimish <48803290+Nimish-0@users.noreply.github.com>
2022-03-07 17:25:37 +0530

    Update WordCountTest.java

--- SparkWordCount/src/test/java/sparkwordcount/WordCountTest.java
+++ SparkWordCount/src/test/java/sparkwordcount/WordCountTest.java
@@ -14,7 +14,8 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+#import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
